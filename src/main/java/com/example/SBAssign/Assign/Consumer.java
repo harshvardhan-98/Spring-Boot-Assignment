030ff1716ea054b8736666203f88583867c709bd
Harsh Vardhan <harsh.vardhan@knoldus.com>
2021-11-25 11:24:08 +0530

    initial commit

---
+++ src/main/java/com/example/SBAssign/Assign/Consumer.java
@@ -0,0 +1,15 @@
+package com.example.SBAssign.Assign;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Consumer {
+
+    @KafkaListener(topics = "test_topic3",groupId = "group_id")
+
+    public void consumerMessage(String message){
+
+        System.out.println(message);
+    }
+}
---
+++ src/main/java/com/example/SBAssign/Assign/Producer.java
@@ -0,0 +1,32 @@
+package com.example.SBAssign.Assign;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class Producer {
+
+    private static final String TOPIC = "test_topic3";
+
+
+    @Autowired
+    private KafkaTemplate<String,String> kafkaTemplate;
+
+    public void sendMessage(String message){
+
+        this.kafkaTemplate.send(TOPIC,message);
+    }
+
+    @Bean
+    public NewTopic createTopic(){
+
+        return new NewTopic(TOPIC,3,(short) 1);
+    }
+
+
+
+}
